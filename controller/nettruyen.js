cdb4cce963f75bc4a82a53b149381c5a94264d8c
chanhuy <luuchanhuy142@gmail.com>
2024-04-10 09:50:58 +0700

    single trust: now every thing is in controller, crawl folder is not needed

--- controller/nettruyen.js
+++ controller/nettruyen.js
@@ -1,8 +1,22 @@
-var getContent = require('../crawl/crawl_truyenfullv2')
+
 var Truyen = require('../models/truyen')
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+var cloudscraper = require('cloudscraper');
+const getHtmlThoughCloudflare = async (url) => {
+    html=await cloudscraper.get(url);
+    return html;
+  }
+  
+const getContent = async (url) => {
+      const html = await getHtmlThoughCloudflare(url)
+      const dom = new JSDOM(html)
+      const content = dom.window.document.querySelector('.chapter-c').textContent
+      return content
+  }
 async function  c1(tentruyen,chapter){
     url='https://truyenfull.vn/'+tentruyen+'/chuong-'+chapter+'/';
-    content= await getContent.getContent(url)
+    content= await getContent(url)
     truyen = new Truyen('nettruyen',content);
 
     
