7e567a9286cfce18eb95715aee0529738206a3a9
chanhuy <luuchanhuy142@gmail.com>
2024-04-23 09:22:42 +0700

    add description

--- models/truyen_detailv2.js
+++ models/truyen_detailv2.js
@@ -1,10 +1,11 @@
 class TruyenDetail2 {
-    constructor(title, cover, url, author, chap) {
+    constructor(title, cover, url, author, chap, description) {
         this.tenTruyen = title;
         this.cover= cover;
         this.url = url;
         this.author = author;
         this.chap = chap;
+        this.description = description;
     }
 }
 
--- truyendetailcontroller/truyen_detail_controllerv2.js
+++ truyendetailcontroller/truyen_detail_controllerv2.js
@@ -23,6 +23,7 @@ const getDetail = async (url) => {
         authors=[]
         chaps=[]
         urls=[]
+        descriptions=[]
         const html = await getHtmlThoughCloudflare(url);
         const dom = new JSDOM(html);
         res=[]
@@ -43,20 +44,25 @@ const getDetail = async (url) => {
           imageurls.push(imageurl[i].getAttribute('data-image'));
           authors.push(author[i].textContent);
           chaps.push(chap[i].textContent);
-          urls.push(title[i].getAttribute('href'));
 
+          const newurl=title[i].getAttribute('href')
+          urls.push(title[i].getAttribute('href'));
+          html2=await getHtmlThoughCloudflare(newurl)
+          const dom2 = new JSDOM(html2);
+          var description = dom2.window.document.querySelector('.desc-text');
+          descriptions.push(description.textContent);
+          
           truyen_detail = new TruyenDetail2(
             titles[i],
             imageurls[i],
             urls[i],
             authors[i],
-            chaps[i]
+            chaps[i],
+            descriptions[i]
           )
-          const newurl=title[i].getAttribute('href')
           
-          html2=await getHtmlThoughCloudflare(newurl)
-          const dom2 = new JSDOM(html2);
-          var description = dom2.window.document.querySelector('.desc-text');
+          
+          
           //console.log(description.textContent);
 
           res.push(truyen_detail)
